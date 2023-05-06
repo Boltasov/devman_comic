1b5faa504200a0dbf1ef9e2c822ea4a905b13ea9
Boltasov <31598564+Boltasov@users.noreply.github.com>
2023-05-06 14:44:39 +0300

    move requests from vk_api_post() into other functions and put vk_base_url into functions

--- main.py
+++ main.py
@@ -5,23 +5,17 @@ import random
 from dotenv import load_dotenv
 
 
-def vk_api_post(vk_base_url, method_name, params):
-    url = f'{vk_base_url}/{method_name}'
-
-    response = requests.post(url, params)
-    response.raise_for_status()
-    return response.json()
-
-
-def get_comic(max_comic_id):
+def upload_random_comic():
+    max_comic_id = requests.get('https://xkcd.com/info.0.json').json()['num']
     image_number = random.randint(1, int(max_comic_id))
     url = f'https://xkcd.com/{image_number}/info.0.json'
 
     response = requests.get(url)
     response.raise_for_status()
 
-    image_link = response.json()['img']
-    comment = response.json()['alt']
+    image_json = response.json()
+    image_link = image_json['img']
+    comment = image_json['alt']
     extension = os.path.splitext(image_link)[1]
     filename = f'{image_number}{extension}'
 
@@ -34,7 +28,8 @@ def get_comic(max_comic_id):
     return filename, comment
 
 
-def get_upload_url(vk_base_url, params):
+def get_upload_url(params):
+    vk_base_url = 'https://api.vk.com/method'
     method_name = 'photos.getWallUploadServer'
     url = f'{vk_base_url}/{method_name}'
 
@@ -53,17 +48,27 @@ def send_photo(file, upload_url, params):
     return response.json()
 
 
-def save_to_album(vk_base_url, photo_response, params):
+def save_to_album(photo_response, params):
+    vk_base_url = 'https://api.vk.com/method'
     method_name = 'photos.saveWallPhoto'
     params.update(photo_response)
+    url = f'{vk_base_url}/{method_name}'
 
-    return vk_api_post(vk_base_url, method_name, params)
+    response = requests.post(url, params)
+    response.raise_for_status()
+
+    return response.json()
 
 
-def post_to_group(vk_base_url, params):
+def post_to_group(params):
+    vk_base_url = 'https://api.vk.com/method'
     method_name = 'wall.post'
+    url = f'{vk_base_url}/{method_name}'
 
-    return vk_api_post(vk_base_url, method_name, params)
+    response = requests.post(url, params)
+    response.raise_for_status()
+
+    return response.json()
 
 
 if __name__ == '__main__':
@@ -71,26 +76,24 @@ if __name__ == '__main__':
     token = os.environ['VK_ACCESS_TOKEN']
     api_version = os.environ['VK_API_VERSION']
     group_id = os.environ['VK_GROUP_ID']
-    vk_url = 'https://api.vk.com/method'
     params = {
         'group_id': group_id,
         'access_token': token,
         'v': api_version,
     }
 
-    max_comic_id = requests.get('https://xkcd.com/info.0.json').json()['num']
     # 0. Download comic from xkcd.com
-    filename, comment = get_comic(max_comic_id)
+    filename, comment = upload_random_comic()
 
     # 1. Get upload url for the image
-    upload_url = get_upload_url(vk_url, params)
+    upload_url = get_upload_url(params)
 
     # 2. Send image to VK
     with open(filename, 'rb') as file:
         photo_response = send_photo(file, upload_url, params)
 
     # 3. Save image to the group album
-    save_response = save_to_album(vk_url, photo_response, params)
+    save_response = save_to_album(photo_response, params)
     media_id = save_response['response'][0]['id']
     owner_id = save_response['response'][0]['owner_id']
 
@@ -103,7 +106,7 @@ if __name__ == '__main__':
         'attachments': f'photo{owner_id}_{media_id}',
         'message': comment,
     }
-    response = post_to_group(vk_url, publish_params)
+    response = post_to_group(publish_params)
 
     # 5. Delete the file
     os.remove(filename)
